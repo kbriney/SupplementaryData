23413041350f72aaff2905e610ff86218ecf08d3
Kristin Briney <briney@caltech.edu>
2022-01-11 13:56:08 -0800

    Cleaned up filtering on description and added columns for url and doi domains.

--- SupplementalAnalysis.R
+++ SupplementalAnalysis.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 library(stringr)
 library(lubridate)
+library(httr)
 
 
 # Relevant file paths, change as necessary
@@ -20,10 +21,14 @@ publications <- read_csv(finput)
 relatedLinks_type <- count(publications, description)
 
 # Filter out related links containing "Data" or "data" and join them into one tibble
-dataLinks_bigD <- filter(publications, str_detect(publications$description, "Data"))
-dataLinks_littleD <-  filter(publications, str_detect(publications$description, "data"))
+dataLinks_bigD <- filter(publications, str_detect(description, "Data"))
+dataLinks_littleD <-  filter(publications, str_detect(description, "data"))
 dataLinks <- union(dataLinks_bigD, dataLinks_littleD)
 
 # Summary counts of types of data links
 dataLinks_type <- count(dataLinks, description)
+
+# Add column for URL domain
+dataLinks <- mutate(dataLinks, related_url_short = str_extract(related_url, "https?://[a-z-\\.]*/"))
+dataLinks <- mutate(dataLinks, related_url_doi = str_extract(related_url, "10\\..*?/"))
 
