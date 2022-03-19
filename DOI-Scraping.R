90895b3b3ecd0a434bbb3f42b619c581cd798b6c
Kristin Briney <briney@caltech.edu>
2022-03-18 17:09:45 -0700

    Got scraping of crossref and datacite DOI prefixes working, with json data in the doi-data folder.

--- DOI-Scraping.R
+++ DOI-Scraping.R
@@ -5,22 +5,26 @@ library(httr)
 #library(rcrossref)
 
 
-fpath <- getwd()
-fname <- "crossref-doi.txt"
-foutput <- paste(fpath, fname, sep="/")
-
+# Formating DOI data correctly
 doi_lookup <- select(dataLinks_doi, related_url_doi)
 doi_lookup <- str_extract(doi_lookup$related_url_doi, "10\\.[0123456789]*")
 doi_lookup <- as_tibble(doi_lookup[!is.na(doi_lookup)])
 
+# Scrape data from CrossRef
 for (i in 1:dim(doi_lookup)[1]) {
-  #crossrefPrefixs <- as.data.frame(cr_prefixes_(doi_lookup[i,1]))
-  #write(crossrefPrefixs, foutput, append=TRUE)
-  
   doi <- as.character(slice(doi_lookup,i))
   jsoninput <- paste("https://api.crossref.org/prefixes/", doi, sep="")
   results <- paste(fpath, "/doi-data/", doi, ".json", sep="")
-  download.file(jsoninput,results, quiet=TRUE)
+  try(download.file(jsoninput,results, quiet=TRUE), silent=TRUE)
 }
+
+# Scrape data from DataCite
+for (i in 1:dim(doi_lookup)[1]) {
+  doi <- as.character(slice(doi_lookup,i))
+  jsoninput <- paste("https://api.datacite.org/prefixes/", doi, sep="")
+  results <- paste(fpath, "/doi-data/", doi, ".json", sep="")
+  try(download.file(jsoninput,results, quiet=TRUE), silent=TRUE)
+}
+
 
 # 10.1093
--- README.md
+++ README.md
@@ -3,3 +3,23 @@ Analysis of supplementary data links in CaltechAUTHORS repository
 
 Author: Kristin A. Briney
 Date: 2022-01
+
+
+Dataset downloaded: 2022-01-06
+
+Needs:
+- Need year data for analysis
+
+Outstanding questions:
+- Are there publications with multiple data links?
+- I'm assuming one link per description (either URL or DOI) so 
+  should not be possible to have both a URL and DOI for one related link record?
+- Do I want to clean and analyze related link descriptions?
+- How to handle outreach for missing data?
+
+Code to write:
+- Scraping DOI prefixes
+  - need to handle 404's
+- Measure decay of URLs and DOIs by counting 404's
+  - how to handle content that's moved?
+- Analyze with respect to date
