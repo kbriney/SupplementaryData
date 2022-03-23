efcf3823b2615ed9df8cf6bd0de6c9a7a6b5cf54
Kristin Briney <briney@caltech.edu>
2022-03-23 16:20:05 -0700

    Parsed scraped JSON files to assign owner information to DOI prefixes and wrote this to a file.

--- 2-DOIScraping.R
+++ 2-DOIScraping.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(stringr)
 library(lubridate)
 library(httr)
-#library(rcrossref)
+library(rjson)
 
 
-# Formating DOI data correctly
+# Formating DOI list correctly
 doi_lookup <- select(dataLinks_doi, related_url_doi)
 doi_lookup <- str_extract(doi_lookup$related_url_doi, "10\\.[0123456789]*")
 doi_lookup <- as_tibble(doi_lookup[!is.na(doi_lookup)])
@@ -18,6 +18,21 @@ for (i in 1:dim(doi_lookup)[1]) {
   try(download.file(jsoninput,results, quiet=TRUE), silent=TRUE)
 }
 
+# Parse crossref names from JSON data
+doi_files <- Sys.glob("doi-data/crossref/*.json")
+doi_files <- as_tibble(doi_files)
+
+crossref <- tibble()
+
+for (i in 1:dim(doi_files)[1]) {
+  doi <- str_extract(doi_files[i,1], "10\\.[0123456789]*")
+  doi_json <- fromJSON(file = as.character(doi_files[i,1]))
+  doi_name <- doi_json$message$name
+  doi_crossref <- tibble("doi" = doi, "doi_name" = doi_name)
+  crossref <- bind_rows(crossref, doi_crossref)
+}
+
+
 # Scrape data from DataCite
 for (i in 1:dim(doi_lookup)[1]) {
   doi <- as.character(slice(doi_lookup,i))
@@ -26,3 +41,39 @@ for (i in 1:dim(doi_lookup)[1]) {
   try(download.file(jsoninput,results, quiet=TRUE), silent=TRUE)
 }
 
+doi_files <- Sys.glob("doi-data/datacite/*.json")
+doi_files <- as_tibble(doi_files)
+
+datacite <- tibble()
+
+for (i in 1:dim(doi_files)[1]) {
+  doi <- str_extract(doi_files[i,1], "10\\.[0123456789]*")
+  doi_json <- fromJSON(file = as.character(doi_files[i,1]))
+  
+  # less specific owner information, always available
+  doi_name <- doi_json$data$relationships$providers$data[[1]]$id
+  # more specific owner information, may not always be available
+  try({doi_name <- doi_json$data$relationships$clients$data[[1]]$id}, silent=TRUE)
+  
+  doi_crossref <- tibble("doi" = doi, "doi_name" = doi_name)
+  datacite <- bind_rows(datacite, doi_crossref)
+}
+
+# Join doi information into one big list
+doi_dupes <- intersect(crossref$doi, datacite$doi)
+datacite_deduped <- filter(datacite, doi != doi_dupes)
+doi_owners <- bind_rows(crossref, datacite_deduped) %>% arrange(doi_name)
+
+# Identify DOIs not found in CrossRef and DataCite
+doi_lookup <- rename(doi_lookup, doi = value)
+doi_noOwners <- as_tibble(setdiff(doi_lookup$doi, doi_owners$doi))
+doi_noOwners <- rename(doi_noOwners, doi = value)
+doi_noOwners <- mutate(doi_noOwners, doi_name = "unknown")
+
+# Combine all information into one large tibble
+doi_owners <- bind_rows(doi_owners, doi_noOwners)
+
+# Write owner information to file
+fpath <- getwd()
+foutput_doi <- paste(fpath, "doi-owners.csv", sep="/")
+write_csv(doi_owners, foutput_doi)
