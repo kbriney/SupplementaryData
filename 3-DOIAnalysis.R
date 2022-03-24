87759c04d62e7b0f7fbfb9b9b5a6a84684ecf355
Kristin Briney <briney@caltech.edu>
2022-03-24 13:55:51 -0700

    Updated DOI CSV file to list owner names instead of prefix numbers. Added metadata header to the script.

--- 3-DOIAnalysis.R
+++ 3-DOIAnalysis.R
@@ -1,3 +1,7 @@
+# An analysis of DOI domains from related data links in CaltechAUTHORS
+# Kristin A. Briney
+# 2022-03
+
 library(tidyverse)
 
 # Relevant file paths, change as necessary
@@ -10,10 +14,10 @@ fname <- "doi-owners.csv"
 finput <- paste(fpath, fname, sep="/")
 doi_own <- read_csv(finput, col_types=cols(doi=col_character(), doi_name=col_character()))
 
-
 # Add DOI name information to larger dataLinks data
 doi_own <- rename(doi_own, related_url_doi = doi)
 dataLinks <- left_join(dataLinks, doi_own, by = "related_url_doi")
+
 
 
 # CatechDATA URLs to convert
@@ -21,5 +25,15 @@ URL_CaltechDATA <- "https://data.caltech.edu/"
 dataLinks_CaltechURLs <- filter(dataLinks, related_url_short == URL_CaltechDATA) %>%
   select(eprint_id, record_doi, related_url, description, type)
 
-foutput_doi <- paste(fpath, "CaltechDATA_URLs.csv", sep="/")
-write_csv(dataLinks_CaltechURLs, foutput_doi)
+foutput <- paste(fpath, "CaltechDATA_URLs.csv", sep="/")
+write_csv(dataLinks_CaltechURLs, foutput)
+
+
+
+# Add names to counts of DOI domains
+dataLinks_doi_name <- count(dataLinks, doi_name) %>% arrange(desc(n))
+
+foutput <- paste(fpath, "supp-data_DOIs.csv", sep="/")
+write_csv(dataLinks_doi_name, foutput)
+
+
