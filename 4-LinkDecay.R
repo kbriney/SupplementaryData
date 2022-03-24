c4efd9efa5d7795ac6aad1d9e95d4e07203bf7f4
Kristin Briney <briney@caltech.edu>
2022-03-24 14:38:00 -0700

    Created script to check links and processed links in proper URL format.

---
+++ 4-LinkDecay.R
@@ -0,0 +1,35 @@
+# An analysis of link stability of related data links in CaltechAUTHORS
+# Kristin A. Briney
+# 2022-03
+
+library(rvest)
+
+# start with new tibble for testing links
+linkDecay <- dataLinks
+
+# Break URLs and DOI's into separate columns
+linkDecay <- mutate(linkDecay, test_URL = str_extract(related_url, "https?://.*"))
+linkDecay <- mutate(linkDecay, test_DOI = str_extract(related_url, "10\\.[0123456789]*/.*"))
+
+# Break URLs and DOI's into separate Tibbles
+#   Note that some URLs contain DOI information, so sorting is done by presence
+#   of URL and not by presence of DOI
+linkDecay_URLs <- filter(linkDecay, !is.na(test_URL))
+linkDecay_DOIs <- filter(linkDecay, is.na(test_URL))
+
+# Remove DOI column, rename URL variable, and assign link type
+linkDecay_URLs <- select(linkDecay_URLs, -test_DOI)
+linkDecay_URLs <- rename(linkDecay_URLs, testLink = test_URL)
+linkDecay_URLs <- mutate(linkDecay_URLs, linkType="URL")
+
+# Reformat DOI to include "https://doi.org/", remove extra columns, assign link type
+doiRoot <- "https://doi.org/"
+linkDecay_DOIs <- mutate(linkDecay_DOIs, testLink = paste(doiRoot, test_DOI, sep=""))
+linkDecay_DOIs <- select(linkDecay_DOIs, -test_DOI, -test_URL)
+linkDecay_DOIs <- mutate(linkDecay_DOIs, linkType="DOI")
+
+# Combine tibbles back into one larger dataset
+linkDecay <- bind_rows(linkDecay_URLs, linkDecay_DOIs)
+
+
+# html_document <- read_html(url)
