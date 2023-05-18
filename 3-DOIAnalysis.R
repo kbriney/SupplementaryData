5b9541567d4e026c706b8430efafd9686a07d49f
Kristin Briney <briney@caltech.edu>
2023-05-18 13:25:38 -0700

    Used output from stage 2 as input for analysis stage 3 instead of assuming data is stored in the environment. Changed name of output file.

--- 3-DOIAnalysis.R
+++ 3-DOIAnalysis.R
@@ -7,13 +7,15 @@ library(tidyverse)
 
 # Relevant file paths, change as necessary
 fpath <- getwd()
-fname <- "2-doi-owners.csv"
 
 # Read doi data file
 # Specifically wrote out and read in file again to be able to add information about
 #   DOI prefixes that are not in CrossRef or DataCite into the CSV file in between processing.
-finput <- paste(fpath, fname, sep="/")
+finput <- paste(fpath, "2-doi-owners.csv", sep="/")
 doi_own <- read_csv(finput, col_types=cols(doi=col_character(), doi_name=col_character()))
+
+finput <- paste(fpath, "1-supp-data.csv", sep="/")
+dataLinks <- read_csv(finput,  col_types=cols(related_url_doi=col_character()))
 
 # Add DOI name information to larger dataLinks data
 doi_own <- rename(doi_own, related_url_doi = doi)
@@ -34,7 +36,7 @@ dataLinks <- left_join(dataLinks, doi_own, by = "related_url_doi")
 # Add names to counts of DOI domains
 dataLinks_doi_name <- count(dataLinks, doi_name) %>% arrange(desc(n))
 
-foutput <- paste(fpath, "3-supp-data_DOIs.csv", sep="/")
+foutput <- paste(fpath, "3-DOI-prefix.csv", sep="/")
 write_csv(dataLinks_doi_name, foutput)
 
 
